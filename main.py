36e509cf6c81675f3b27970767e098df071fa59d
Jack <zxcqwe490649064906@gmail.com>
2023-05-21 11:31:43 +0800

    Add tic-tac-toe game
    - add front-end and backend
    - add reset button
    - add related styles

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import Optional
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -8,8 +9,26 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def get(request: Request):
+def get(request: Request, board: Optional[str] = "000000000"):
+    count_dict = {'1': 0, '2': 0}
+    for c in board:
+        if c == '1':
+            count_dict['1'] += 1
+        elif c == '2':
+            count_dict['2'] += 1
+
+    if count_dict['1'] == count_dict['2']:
+        turn = '1'
+    elif count_dict['1'] - count_dict['2'] == 1:
+        turn = '2'
+    else:
+        raise ValueError("board invalid")
+
     return templates.TemplateResponse(
         "tic_tac_toe.html",
-        {"request": request}
+        {
+            "request": request,
+            "board": board,
+            "turn": turn,
+        }
     )
